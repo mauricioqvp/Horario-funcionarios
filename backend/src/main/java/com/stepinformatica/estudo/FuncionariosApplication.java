34671d1548157aef6c67f5fa6547abdf390932d3
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2021-04-25 21:36:20 -0300

    Project created

---
+++ backend/src/main/java/com/stepinformatica/estudo/FuncionariosApplication.java
@@ -0,0 +1,12 @@
+package com.stepinformatica.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuncionariosApplication.class, args);
+	}
+}
